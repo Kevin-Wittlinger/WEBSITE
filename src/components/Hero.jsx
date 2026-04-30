5267d6f6d0018490089b041cf4bbf78857ed8264
Kevin Wittlinger <124730586+Kevin-Wittlinger@users.noreply.github.com>
2026-04-30 16:43:02 -0400

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,7 +19,7 @@ const Hero = () => {
           <h1
             className={`${styles.heroHeadText}
           text - white`}>
-            HELLO VISITOR, I'm <span className="text-[915eff">Kevin Wittlinger</span>
+            HELLO, I'm <span className="text-[915eff">Kevin Wittlinger</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             Welcome to my portfolio website where you can learn everything about Kevin Wittlinger!
